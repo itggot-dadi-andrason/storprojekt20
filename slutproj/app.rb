b5cad8d304ac43758c264998a46a629838e873fb
itggot-dadi-andrason <59963408+itggot-dadi-andrason@users.noreply.github.com>
2020-02-06 15:23:34 +0100

    02/06
    yes

--- slutproj/app.rb
+++ slutproj/app.rb
@@ -73,19 +73,56 @@ get('/uploadtest') do
     slim(:upload)
 end
 
-post('/upload') do
-    unless params[:file] &&
-           (tempfile = params[:file][:tempfile]) &&
-           (name = params[:file][:filename])
-      @error = "No file selected"
-      return slim(:upload)
+def fileupload(filename)
+    unless filename &&
+        (tempfile = filename[:tempfile]) &&
+        (name = filename[:filename])
+    @error = "No file selected"
+    return slim(:upload)
     end
-    puts "Uploading file, original name #{name.inspect}"
-    target = "public/img/#{name}"
-    slimroute = "img/#{name}"
+    p filename
+    fileextension = filename["filename"]
+    if File.extname("#{fileextension}") == ".png" or File.extname("#{fileextension}") == ".jpg" or File.extname("#{fileextension}") == ".jpeg" or File.extname("#{fileextension}") == ".gif"
+        puts "Uploading file, original name #{name.inspect}"
+        target = "public/img/#{name}"
+        slimroute = "img/#{name}"
+        files = {target: target, slimroute: slimroute, tempfile: tempfile}
+        return files
+    else
+        redirect('/wrongext')
+    end
+end
+
+post('/upload') do
+    # unless params[:file] &&
+    #        (tempfile = params[:file][:tempfile]) &&
+    #        (name = params[:file][:filename])
+    #   @error = "No file selected"
+    #   return slim(:upload)
+    # end
+    # puts "Uploading file, original name #{name.inspect}"
+    # target = "public/img/#{name}"
+    # slimroute = "img/#{name}"
+    files = fileupload(params[:file])
+    p files
+    target = files[:target]
+    p target
+    tempfile = files[:tempfile]
+    p params[:file][:tempfile]
+    slimroute = files[:slimroute]
     db.execute("UPDATE users SET avatar=? WHERE user_id=#{session[:user_id]}", slimroute)
     File.open(target, 'wb') {|f| f.write tempfile.read }
+    filesize = File.size(target)
+    if File.size(target) > 2000000
+        File.delete(target)
+        redirect('/filebig')
+    end
+    p filesize
     redirect('/webshop')
+end
+
+get('/filebig') do
+    slim(:filebig)
 end
 
 get('/webshop') do
